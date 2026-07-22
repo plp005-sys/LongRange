4252371bb0dc1527238271150dda65bf38f62521
plp005-sys <csia005@gmail.com>
2026-07-22 04:38:59 -0600

    upT

--- server.ts
+++ server.ts
@@ -222,9 +222,12 @@ async function startServer() {
     });
   }
 
-  app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
+  if (!process.env.VERCEL) {
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  }
+  return app;
 }
 
-startServer();
+export default startServer();
